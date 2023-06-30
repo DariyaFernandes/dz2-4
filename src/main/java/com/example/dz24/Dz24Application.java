490cf89be29eca7d174390e82af08180e6777d0f
ааа <https://github.com/@DariyaFernandes/Homework1/pull/1>
2023-06-30 08:06:11 +0300

    dz2-4

---
+++ src/main/java/com/example/dz24/Dz24Application.java
@@ -0,0 +1,13 @@
+package com.example.dz24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz24Application.class, args);
+    }
+
+}
